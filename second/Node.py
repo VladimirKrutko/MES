e4288e3553c5d2afca0da7fd329619bd63695230
VladimirKrutko <krutkovova24@gmail.com>
2024-01-10 14:43:19 +0100

    working second version

--- second/Node.py
+++ second/Node.py
@@ -1,12 +1,14 @@
-from Calculation2 import Calculation
+from second.Calculation import Calculation
 
 class Node:
-    def __init__(self, x,y, pc, data):
+    def __init__(self, x,y, pc, data, L_ind, pc_number):
         self.x = x
         self.y = y
         self.pc = pc
+        self.L_ind = L_ind
         self.data = data
-        self.surface = Calculation(2).surfaces(self.x, self.y, self.pc, self.data['Alfa'], self.data['Tot'], self.data, self.data['Conductivity'])
+        self.surface = Calculation(pc_number).surfaces(self.x, self.y, self.pc, self.data['Alfa'], 
+                                              self.data['Tot'], self.data, self.L_ind, self.data['Conductivity'])
 
     def __str__(self):
         return f"x => {' '.join(self.x)} \n y=> { ' '.join(self.y)}"
